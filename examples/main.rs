b88e2b1b8da6f3ec48c613de8992b590bd018a8c
Maksim Sch <maks777s@gmail.com>
2021-11-12 00:32:48 +0300

    infinite loop

--- examples/main.rs
+++ examples/main.rs
@@ -1,29 +1,26 @@
-use std::sync::{Arc, Mutex};
+use chrono::prelude::DateTime;
+use chrono::Utc;
+use pcap::Device;
 use std::io::stdin;
+use std::iter::FromIterator;
+use std::path;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time;
-use std::path;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use structopt::StructOpt;
 use warp::Filter;
 use zwift_capture::{Player, ZwiftCapture};
-use zwift_watcher::{World, PlayerGroup, PlayerData, PLAYER_GROUP_CAPACITY};
 use zwift_watcher::server::{handlers, models, Routes};
-use std::iter::FromIterator;
-use pcap::Device;
-use structopt::StructOpt;
-use chrono::prelude::DateTime;
-use std::time::{Duration,UNIX_EPOCH,SystemTime};
-use chrono::Utc;
-
+use zwift_watcher::{PlayerData, PlayerGroup, World, PLAYER_GROUP_CAPACITY};
 
 const TICK: i64 = 1000;
 
-
-#[derive(StructOpt,Debug,Clone)]
+#[derive(StructOpt, Debug, Clone)]
 struct Cli {
     dump_file: String,
-    sleep: u64
+    sleep: u64,
 }
-
 
 #[tokio::main]
 async fn main() {
@@ -43,7 +40,8 @@ async fn main() {
         let mut counter: i64 = 0;
         println!("Capture thread: start");
 
-        for players in &mut capture { // .skip(20000) {
+        for players in &mut capture {
+            // .skip(20000) {
             let mut world_capture = world_capture.lock().unwrap();
             let _times = world_capture.push_players_batch(players).unwrap();
 
@@ -54,10 +52,17 @@ async fn main() {
             if counter % TICK == 0 {
                 let st = UNIX_EPOCH + Duration::from_millis(world_capture.world_time as u64);
                 let datetime = DateTime::<Utc>::from(st);
-                println!("Tick {}, time: [{}] {}", counter, world_capture.world_time, datetime.format("%Y-%m-%d %H:%M:%S.%f").to_string());
+                println!(
+                    "Tick {}, time: [{}] {}",
+                    counter,
+                    world_capture.world_time,
+                    datetime.format("%Y-%m-%d %H:%M:%S.%f").to_string()
+                );
                 if let Some(outdated) = world_capture.find_outdated_players() {
                     println!("Outdated players: {}", outdated.len());
-                    outdated.iter().for_each(|&id| world_capture.clear_player(id))
+                    outdated
+                        .iter()
+                        .for_each(|&id| world_capture.clear_player(id))
                 }
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-use std::sync::{Arc, Mutex};
+use chrono::prelude::DateTime;
+use chrono::Utc;
+use pcap::Device;
 use std::io::stdin;
+use std::iter::FromIterator;
+use std::path;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time;
-use std::path;
-use std::iter::FromIterator;
-use pcap::Device;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use warp::Filter;
 use zwift_capture::{Player, ZwiftCapture};
-use zwift_watcher::{World, PlayerGroup, PlayerData, PLAYER_GROUP_CAPACITY};
-use zwift_watcher::server::{handlers,models,Routes};
-
+use zwift_watcher::server::{handlers, models, Routes};
+use zwift_watcher::{PlayerData, PlayerGroup, World, PLAYER_GROUP_CAPACITY};
 
 const TICK: i64 = 1000;
 
@@ -19,42 +21,53 @@ async fn main() {
     let world = Arc::new(Mutex::new(World::new()));
     let world_capture = world.clone();
 
-    // real capture device
-    let mut devices_list = Device::list().unwrap();
-    for (ix, device) in devices_list.iter().enumerate() {
-        let desc = match &device.desc {
-            Some(v) => v.clone(),
-            _ => String::from("---")
-        };
-        println!("{}: {} {:?}", ix, &device.name ,desc);
-    }
-    println!("\nPlease choose device:");
-    let mut input_str = String::new();
-    stdin().read_line(&mut input_str).expect("invalid value");
-    let choice: usize = input_str.trim().parse().unwrap();
-    let selected_device = devices_list.remove(choice);
-
-    println!("Selected device: {:?}", selected_device);
-    let mut capture = ZwiftCapture::from_device(selected_device);
-
     let capture_thread = thread::spawn(move || {
         let mut counter: i64 = 0;
         println!("Capture thread: start");
+        // real capture device
+        let mut devices_list = Device::list().unwrap();
+        for (ix, device) in devices_list.iter().enumerate() {
+            let desc = match &device.desc {
+                Some(v) => v.clone(),
+                _ => String::from("---"),
+            };
+            println!("{}: {} {:?}", ix, &device.name, desc);
+        }
+        println!("\nPlease choose device:");
+        let mut input_str = String::new();
+        stdin().read_line(&mut input_str).expect("invalid value");
+        let choice: usize = input_str.trim().parse().unwrap();
+        let selected_device = devices_list.remove(choice);
 
-        for players in &mut capture { // .skip(20000) {
-            let mut world_capture = world_capture.lock().unwrap();
-            let _times = world_capture.push_players_batch(players).unwrap();
+        loop {
+            println!("Open device: {:?}", &selected_device);
+            let mut capture = ZwiftCapture::from_device(selected_device.clone());
+            for players in &mut capture {
+                // .skip(20000) {
+                let mut world_capture = world_capture.lock().unwrap();
+                let _times = world_capture.push_players_batch(players).unwrap();
 
-            counter += 1;
-            if counter % TICK == 0 {
-                println!("Tick {}, time: {}", counter, world_capture.world_time);
-                if let Some(outdated) = world_capture.find_outdated_players() {
-                    println!("Outdated players: {}", outdated.len());
-                    outdated.iter().for_each(|&id| world_capture.clear_player(id))
+                counter += 1;
+                if counter % TICK == 0 {
+                    let st = UNIX_EPOCH + Duration::from_millis(world_capture.world_time as u64);
+                    let datetime = DateTime::<Utc>::from(st);
+                    println!(
+                        "Tick {}, time: [{}] {}",
+                        counter,
+                        world_capture.world_time,
+                        datetime.format("%Y-%m-%d %H:%M:%S.%f").to_string()
+                    );
+                    if let Some(outdated) = world_capture.find_outdated_players() {
+                        println!("Outdated players: {}", outdated.len());
+                        outdated
+                            .iter()
+                            .for_each(|&id| world_capture.clear_player(id))
+                    }
                 }
             }
+            capture.print_stat();
+            println!("Close device");
         }
-        capture.print_stat();
         println!("Capture thread: done")
     });
 
